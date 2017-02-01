199f2c3d4a6d26f9db6c0da0dd45cf01a774df93
Rob Hitt <robhitt@gmail.com>
2017-02-01 09:20:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const app = "I don't do much.";
+const rob = "I do much.";
+//
+// export default class MovieBrowser extends React.Component {
+//   render() {
+//     const currentPlayingTitle = 'Mad Max: Fury Road';
+//     const childrenWithExtraProp = React.Children.map(this.props.children, child => {
+//       return React.cloneElement(child, {
+//         isPlaying: child.props.title === currentPlayingTitle
+//       });
+//     });
+//
+//     return (
+//       <div className="movie-browser">
+//         {childrenWithExtraProp}
+//       </div>
+//     );
+//   }
+// }
